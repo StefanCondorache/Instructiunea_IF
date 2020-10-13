1d4d6000f2d9537c7f377abe21552504ed372ae9
StefanCondorache <70690185+StefanCondorache@users.noreply.github.com>
2020-10-13 10:45:59 +0300

    Add files via upload

---
+++ Problema_1_IF.py
@@ -0,0 +1,21 @@
+n1=int(input("nr crt "))
+p1=int(input("punctaj"))
+n2=int(input("nr crt"))
+p2=int(input("punctaj"))
+n3=int(input("nr crt"))
+p3=int(input("punctaj"))
+if (p1>0) and (p2>0) and (p3>0):
+    if (p1>p2) and (p1>p3):
+        print("punctaj maxim are nr crt",n1)
+    elif (p2>p1) and (p2>p3):
+        print("punctaj maxim are nr crt",n2)
+    elif (p3>p1) and (p3>p2):
+        print("punctaj maxim are nr crt",n3)
+    elif (p1==p2) and (p1>p3):
+        print("punctaj maxim are nr crt",n1,n2)
+    elif (p1==p3) and (p1>p2):
+        print("punctaj maxim are nr crt",n1,n3)
+    elif (p3==p2) and (p3>p1):
+        print("punctaj maxim are nr crt",n2,n3)
+else: 
+    print("punctaj negativ")
