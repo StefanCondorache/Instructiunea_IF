521c61702122ea353d881d8ab8ca9578be87efa1
StefanCondorache <70690185+StefanCondorache@users.noreply.github.com>
2020-10-14 18:33:22 +0300

    Add files via upload

---
+++ Problema_4_IF.py
@@ -0,0 +1,11 @@
+# Se introduc vârstele a 3 persoane. Afişaţi vârstele cuprinse între 18 şi 60 de ani. 
+# Exemplu : Date de intrare 56 34 12 Date de ieşire 56 34.
+a=int(input("a="))
+b=int(input("b="))
+c=int(input("c="))
+if (a>18) and (a<60):
+    print(a)
+if (b>18) and (b<60):
+    print(b)
+if (c>18) and (c<60):
+    print(c)
