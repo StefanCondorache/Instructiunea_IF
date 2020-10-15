1aa597045562275318d9c71eccb433c752a8e6e7
StefanCondorache <70690185+StefanCondorache@users.noreply.github.com>
2020-10-15 20:49:21 +0300

    Update Problema_3_IF.py

--- Problema_3_IF.py
+++ Problema_3_IF.py
@@ -1,8 +1,11 @@
 # Să se verifice dacă o literă introdusă este vocală sau consoană. 
 # Exemplu : Date de intrare a Date de ieşire vocala.
-
 l=input("litera ")
-elif (l=="a") or (l=="e") or (l=="i") or( l=="o") or (l=="u") or (lit == 'ă') or (lit == 'î') or (lit == 'â'):
+list1=["a","e","i","o","u","ă","î","â","A","E","I","O","U","Ă","Î","Â"]
+list2=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','ș','t','ț','v','w','x','y','z','B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','Ș','T','Ț','V','W','X','Y','Z']
+if l in list1:
     print("vocală")
-else:
+elif l in list2:
     print("consoană")
+else:
+    print("caracter greșit")
