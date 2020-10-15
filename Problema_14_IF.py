74413b9b66b93cfd0083e6147873034ab3eb4ac8
StefanCondorache <70690185+StefanCondorache@users.noreply.github.com>
2020-10-15 21:03:07 +0300

    Add files via upload

---
+++ Problema_14_IF.py
@@ -0,0 +1,5 @@
+# Într-o tabără, băieţii sunt cazaţi câte 4 într-o căsuţă, în ordinea sosirii. Ionel a sosit al n-lea. În a câta căsuţă se va afla?
+# Exemplu : date de intrare : n=69 date de ieşire : casuta 17.
+n=int(input("n="))
+if n>0:
+    print("casuta",n//4)
